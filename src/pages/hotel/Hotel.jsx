5cdc894fd735ddf8fba891ccff49d02713f05855
yıldız <mesel4461@gmail.com>
2023-07-12 21:14:23 +0300

    as

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -24,7 +24,9 @@ const Hotel = () => {
   const [open, setOpen] = useState(false);
   const [openModal, setOpenModal] = useState(false);
 
-  const { data, loading, error } = useFetch(`/hotels/find/${id}`);
+  const { data, loading, error } = useFetch(
+    `http://localhost:8800/api/hotels/find/${id}`
+  );
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
