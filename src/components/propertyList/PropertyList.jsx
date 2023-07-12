2d9be81cb84b40343d5f62f2a25acce7bc874b7e
yıldız <mesel4461@gmail.com>
2023-07-12 17:22:02 +0300

    up

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -5,6 +5,7 @@ const PropertyList = () => {
   const { data, loading, error } = useFetch(
     "http://localhost:8800/api/hotels/countByType"
   );
+
   const images = [
     "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
@@ -15,23 +16,21 @@ const PropertyList = () => {
   return (
     <div className="pList">
       {loading ? (
-        "loadding"
+        "loading"
       ) : (
         <>
           {data &&
-            images.map(() => {
-              <div className="pListItem">
-                <img
-                  src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
-                  alt=""
-                  className="pListImg"
-                />
+            images.map((img, i) => (
+              <div className="pListItem" key={i}>
+                <img src={img} alt="" className="pListImg" />
                 <div className="pListTitles">
-                  <h1>Hotels</h1>
-                  <h2>233 hotels</h2>
+                  <h1>{data[i]?.type}</h1>
+                  <h2>
+                    {data[i]?.count} {data[i]?.type}
+                  </h2>
                 </div>
-              </div>;
-            })}
+              </div>
+            ))}
         </>
       )}
     </div>
